0efdf3443e2ab01aeb75b51e644dc0ad20555d65
Kiril Vatev <vatev.1@gmail.com>
2025-06-28 18:45:24 -0400

    cleanup

--- web/components/Debug.js
+++ web/components/Debug.js
@@ -9,20 +9,20 @@ function getSupportedMediaCodecs() {
   const audioContext = new (window.AudioContext || window.webkitAudioContext)(); // For audio codecs
 
   const videoCodecs = [
-    'video/mp4; codecs="avc1.42E01E"', // H.264 Baseline Profile
+    'video/mp4; codecs="avc1.42E01E"',
     'video/mp4; codecs="vp8"',
     'video/mp4; codecs="vp9"',
-    'video/webm; codecs="vp8"',       // VP8
-    'video/webm; codecs="vp9"',       // VP9
-    'video/mp4; codecs="hvc1.1.6.L90.B0"', // HEVC (H.265)
-    'video/mp4; codecs="av01.0.05M.08"', // AV1
+    'video/webm; codecs="vp8"',
+    'video/webm; codecs="vp9"',
+    'video/mp4; codecs="hvc1.1.6.L90.B0"',
+    'video/mp4; codecs="av01.0.05M.08"',
   ];
 
   const audioCodecs = [
-    'audio/mpeg',                     // MP3
-    'audio/ogg; codecs="vorbis"',     // Vorbis
-    'audio/ogg; codecs="opus"',       // Opus
-    'audio/aac',                      // AAC
+    'audio/mpeg',
+    'audio/ogg; codecs="vorbis"',
+    'audio/ogg; codecs="opus"',
+    'audio/aac',
   ];
 
   return [...videoCodecs, ...audioCodecs].reduce((memo, codec) => {
